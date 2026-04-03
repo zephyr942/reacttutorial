a627e108c7fc99875261a03ef0fdf181bf68198d
zephyr942 <bche942@aucklanduni.ac.nz>
2026-04-03 21:11:09 +1300

    5-10 Showing a loading indicator

--- src/components/Practice.tsx
+++ src/components/Practice.tsx
@@ -9,23 +9,30 @@ interface User {
 const Practice = () => {
   const [users, setUsers] = useState<User[]>();
   const [error, setError] = useState("");
+  const [isLoding, setLoading] = useState(false);
 
   useEffect(() => {
     const controller = new AbortController(); //avoid that the request send to the server twice
+    setLoading(true);
     axios
       .get<User[]>("https://jsonplaceholder.typicode.com/users", {
         signal: controller.signal,
       })
-      .then((res) => setUsers(res.data))
+      .then((res) => {
+        setUsers(res.data);
+        setLoading(false);
+      })
       .catch((err) => {
         if (err instanceof CanceledError) return; //don't show the first cancel text
         setError(err.message);
+        setLoading(false);
       });
     return () => controller.abort();
   }, []);
 
   return (
     <>
+      {isLoding && <div className="spinner-border"></div>}
       {error && <p className="text-danger">{error}</p>}
       {users?.map((user) => (
         <li key={user.id}>{user.name}</li>
