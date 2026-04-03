d1a99b54b718432ab7f061e5a30c74c3101bdb91
zephyr942 <bche942@aucklanduni.ac.nz>
2026-04-03 22:25:48 +1300

    5-13 Updating Data

--- src/components/Practice.tsx
+++ src/components/Practice.tsx
@@ -50,11 +50,27 @@ const Practice = () => {
 
     axios
       .post("https://jsonplaceholder.typicode.com/users", newUser)
-      .then(({ data: saveUser }) => {//destructed the response to {data}  = const {data} = response
+      .then(({ data: saveUser }) => {
+        //destructed the response to {data}  = const {data} = response
         setUsers([saveUser, ...users]);
       })
       .catch((err) => {
         setError(err.mesage);
+        setUsers(originalUsers);
+      });
+  };
+
+  const updateUser = (user: User) => {
+    const originalUsers = [...users];
+    const updatedUser = { ...user, name: user.name + "!" };
+    setUsers(users.map((u) => (u.id === user.id ? updatedUser : u)));
+    axios
+      .patch(
+        "https://jsonplaceholder.typicode.com/users/" + user.id,
+        updatedUser,
+      )
+      .catch((err) => {
+        setError(err.message);
         setUsers(originalUsers);
       });
   };
@@ -73,12 +89,20 @@ const Practice = () => {
             className="list-group-item d-flex justify-content-between"
           >
             {user.name}
-            <button
-              className="btn btn-outline-danger"
-              onClick={() => deleteUser(user)}
-            >
-              Delete
-            </button>
+            <div>
+              <button
+                className="btn btn-outline-secondary mx-1"
+                onClick={() => updateUser(user)}
+              >
+                Update
+              </button>
+              <button
+                className="btn btn-outline-danger"
+                onClick={() => deleteUser(user)}
+              >
+                Delete
+              </button>
+            </div>
           </li>
         </ul>
       ))}
